7ed36e19b6132771914e34e626bca4fbf936d047
Terry Johnson <giovanni7304@me.com>
2016-10-11 16:23:24 -0500

    Added the center Compass and the generate Pokemon on the screen in random locations near the users current location

--- Pokeman/MapViewController.swift
+++ Pokeman/MapViewController.swift
@@ -14,6 +14,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    let stdRadius = 200 as CLLocationDistance
     var manager = CLLocationManager()
     var updateCount = 0
     
@@ -27,12 +28,24 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
             print("Ready to Go")
             mapView.showsUserLocation = true
             manager.startUpdatingLocation()
+            
+                Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (timer) in
+                //Spawn a Pokeman
+                    if let coord = self.manager.location?.coordinate {
+                        let anotate = MKPointAnnotation()
+                        anotate.coordinate = coord
+                        let randoLat = (Double(arc4random_uniform(200)) - 100.0) / 50000.0
+                        let randoLon = (Double(arc4random_uniform(200)) - 100.0) / 50000.0
+                        anotate.coordinate.latitude += randoLat
+                        anotate.coordinate.longitude += randoLon
+                        self.mapView.addAnnotation(anotate)
+                    }
+            })
         } else {
             manager.requestWhenInUseAuthorization()
         }
         
         manager.requestWhenInUseAuthorization()
-        
         mapView.showsUserLocation = true
     }
 
@@ -45,11 +58,19 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         print("We Made It")
         
         if updateCount < 3 {
-            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 1000, 1000)
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, stdRadius, stdRadius)
             mapView.setRegion(region, animated: false)
             updateCount += 1
+        } else {
+            manager.stopUpdatingLocation()
         }
     }
 
+    @IBAction func centerTapped(_ sender: AnyObject) {
+        if let coord = manager.location?.coordinate {
+            let region = MKCoordinateRegionMakeWithDistance(coord, stdRadius, stdRadius)
+            mapView.setRegion(region, animated: true)
+        }
+    }
 }
 
